5af119d24fc6f6d33a279c76441bb19da24949b7
Taha Sayyed <codetaha786@gmail.com>
2025-11-27 12:05:07 +0530

    Add recursion demo

---
+++ Recursion/basic_recursion_demo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+void func(int n){
+    if(n>0){
+        cout<<n<<" "; //Execute in calling phase
+        func(n-1);
+    }
+}
+
+int main(){
+    func(5);
+    return 0;
+}
