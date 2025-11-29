af42e0943edf5844baeee4b70d8ec0acfc8a7981
Taha Sayyed <codetaha786@gmail.com>
2025-11-29 13:10:11 +0530

    Add transposition for linear search

---
+++ Array/linear_search_transposition.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<utility>
+
+
+using namespace std;
+
+class ArrayHandler{
+    private:
+        int arr[100]; //Fixed Size array
+        int length; //Current number of elements
+
+    public:
+        ArrayHandler(){
+            length=0;
+        }
+
+        void setElement(int input_arr[],int input_arr_length){
+            length=input_arr_length;
+            for(int i=0;i<input_arr_length;i++){
+                arr[i]=input_arr[i];
+            }
+        }
+        
+
+        //Transposition technique
+        int linearSearch(int key){
+            for(int i=0;i<length;i++){
+                if(arr[i]==key){
+                    swap(arr[i],arr[i-1]);
+                    return i-1;
+                }
+            }
+            return -1;
+        }
+
+        void display(){
+            for(int i=0;i<length;i++){
+                cout<<arr[i]<<" ";
+            }
+        }
+};
+
+int main(){
+    int arr[5]={10,20,30,40,50};
+    ArrayHandler arr_1;
+    arr_1.setElement(arr,5);
+    cout<<"Element found at "<<arr_1.linearSearch(50)<<endl;
+    arr_1.display();
+    
+    cout<<"\nElement found at "<<arr_1.linearSearch(50)<<endl;
+    arr_1.display();
+    
+    return 0;
+}
