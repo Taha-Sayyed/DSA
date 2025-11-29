1a4636277cc77e3b0ae0924237f617f1b6aaf533
Taha Sayyed <codetaha786@gmail.com>
2025-11-29 13:23:07 +0530

    Add move front technique in linear search

---
+++ Array/linear_search_moveFront.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<utility>
+
+
+using namespace std;
+
+class ArrayHandler{
+    private:
+        int arr[100]; //Fixed Size array
+        int length; //Current number of elements
+
+    public:
+        ArrayHandler(){
+            length=0;
+        }
+
+        void setElement(int input_arr[],int input_arr_length){
+            length=input_arr_length;
+            for(int i=0;i<input_arr_length;i++){
+                arr[i]=input_arr[i];
+            }
+        }
+        
+
+        //Move to Front
+        int linearSearch(int key){
+            for(int i=0;i<length;i++){
+                if(arr[i]==key){
+                    swap(arr[i],arr[0]);
+                    return 0;
+                }
+            }
+            return -1;
+        }
+
+        void display(){
+            for(int i=0;i<length;i++){
+                cout<<arr[i]<<" ";
+            }
+        }
+};
+
+int main(){
+    int arr[5]={10,20,30,40,50};
+    ArrayHandler arr_1;
+    arr_1.setElement(arr,5);
+    cout<<"Element found at "<<arr_1.linearSearch(50)<<endl;
+    arr_1.display();
+
+    //Now if the same element is again searched then it will take less time then before
+
+    cout<<"\nElement found at "<<arr_1.linearSearch(50)<<endl;
+    arr_1.display();
+    
+    return 0;
+}
