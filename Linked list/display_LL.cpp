21267fecbf41cf3dc485551766e68d4f67f61e31
Taha Sayyed <codetaha786@gmail.com>
2025-12-21 10:42:33 +0530

    Add display_LL.cpp

---
+++ Linked list/display_LL.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        class Node *next;
+};
+
+class Node *first=NULL; //global variable
+
+void create(int A[],int n){
+    int i;
+    struct Node *t,*last;
+    first=new Node;
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+
+    for(int i=1;i<n;i++){
+        t=new Node;
+        t->data=A[i];
+        t->next=0;
+        last->next=t;
+        last=t;
+    }
+}
+
+void display(class Node *p){
+    while(p!=0){
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+
+int main(){
+
+    int A[]={10,20,30,40};
+    create(A,4);
+    display(first);
+
+
+    return 0;
+}
