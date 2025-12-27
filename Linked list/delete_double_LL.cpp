48bc5c54feba2a439eb4d7126ab8a0e5d91a7d95
Taha Sayyed <codetaha786@gmail.com>
2025-12-27 11:29:03 +0530

    Add delete double LL.cpp

---
+++ Linked list/delete_double_LL.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        class Node *prev;
+        int data;
+        class Node *next;
+};
+
+class Node *Head=NULL;
+
+void create(int A[],int n){
+    Head=new Node;
+    class Node *t=NULL;
+    class Node *last=NULL;
+    last=Head;
+
+    Head->data=A[0];
+    Head->prev=NULL;
+    Head->next=NULL;
+
+    for(int i=1;i<n;i++){
+        t=new Node;
+        t->data=A[i];
+        last->next=t;
+        t->prev=last;
+        t->next=NULL;
+        last=t;
+    }
+}
+
+
+void display(class Node *p){
+    while (p!=0){
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+}
+
+
+void deleteNode(int pos){
+    
+    int x;
+    class Node *p=Head;
+    
+    if(pos<=0){
+        cout<<"Invalid position"<<endl;
+    }
+    if(pos==1){
+        x=p->data;
+        if(Head->next!=NULL){
+            Head=Head->next;
+            Head->prev=NULL;
+        }
+        delete p;
+    }else{
+        for(int i=1;i<pos;i++){
+            p=p->next;
+        }
+        
+        p->prev->next=p->next;
+        if(p->next!=NULL){
+            p->next->prev=p->prev;
+            delete p;
+        }else{
+            p->prev=NULL;
+            delete p;
+        }
+    }
+}
+
+int main(){
+
+    int A[]={10,20,30,40,50};
+    create(A,5);
+    deleteNode(5);
+    display(Head);
+
+    return 0;
+}
